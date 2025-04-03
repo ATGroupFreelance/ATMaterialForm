e85c7f568602374d60afdca5ca3371ee3265c588
ATFreelance <ATGroupfreelance@gmail.com>
2025-04-03 16:07:04 +0330

    updated table component

--- src/lib/component/ATForm/UI/Table/Table.jsx
+++ src/lib/component/ATForm/UI/Table/Table.jsx
@@ -1,75 +1,11 @@
 import React from 'react';
+import VerticalTable from './VerticalTable/VerticalTable';
+import HorizontalTable from './HorizontalTable/HorizontalTable';
 
-import MUITable from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
-//Hooks
-import useATFormProvider from '../../../../hooks/useATFormProvider/useATFormProvider';
+const Table = ({ tableType = 'Vertical', ...restProps }) => {
+    console.log('tableType', tableType)
 
-const Table = ({ atFormProvidedProps, id, data, columns, label, cellStyle, labelStyle, columnCellStyle, hideColumns }) => {
-    const { getLocalText } = useATFormProvider()
-
-    const newColumns = [
-        ...(columns || [])
-    ]
-
-    if (!newColumns.length && data && data.length) {
-        for (let key in data[0]) {
-            newColumns.push(key)
-        }
-    }
-
-    const newData = data || []
-
-    return <TableContainer component={Paper}>
-        {
-            label && label !== '' &&
-            <Typography sx={{ fontWeight: 'bold', ...(labelStyle || {}) }}>
-                {getLocalText(label)}
-            </Typography>
-        }
-        <MUITable sx={{ minWidth: 650 }} aria-label={label}>
-            <TableHead>
-                {
-                    !hideColumns
-                    &&
-                    <TableRow>
-                        {
-                            newColumns.map(item => {
-                                return <TableCell key={item} sx={columnCellStyle || {}}>{getLocalText(item)}</TableCell>
-                            })
-                        }
-                    </TableRow>
-                }
-            </TableHead>
-            <TableBody>
-                {
-                    newData.map((row, index) => {
-                        const cells = []
-
-                        for (let key in row) {
-                            cells.push(
-                                <TableCell key={key} sx={cellStyle || {}}>
-                                    {row[key]}
-                                </TableCell>
-                            )
-                        }
-
-                        return <TableRow key={JSON.stringify(row) + index}>
-                            {
-                                cells
-                            }
-                        </TableRow>
-                    })
-                }
-            </TableBody>
-        </MUITable>
-    </TableContainer>
+    return tableType === 'Vertical' ? <VerticalTable {...restProps} /> : <HorizontalTable {...restProps} />
 }
 
 export default Table;
