876565df53a464d4c84071e3dda71bcee98e4d4b
ATFreelance <ATGroupfreelance@gmail.com>
2025-08-25 11:02:46 +0330

    small fix

--- src/lib/component/ATForm/ATFormTemplateWrappers/ATFormButtonDialogWrapper/ATFormButtonDialogWrapperDialog/ATFormButtonDialogWrapperDialog.tsx
+++ src/lib/component/ATForm/ATFormTemplateWrappers/ATFormButtonDialogWrapper/ATFormButtonDialogWrapperDialog/ATFormButtonDialogWrapperDialog.tsx
@@ -7,18 +7,13 @@ const ATFormButtonDialogWrapperDialog = ({ children, onClose, onSubmitClick, onR
 
     return <Dialog open={true} onClose={onClose} maxWidth={"md"} fullWidth>
         <DialogTitle>
-            
+
         </DialogTitle>
         <DialogContent dividers>
             {children}
         </DialogContent>
         <DialogActions>
-            <Grid container spacing={2} sx={{ width: "100%" }}>
-                <Grid size={{ xs: 12, md: 2 }}>
-                    <Button onClick={onSubmitClick} color="success">
-                        {getLocalText('Save')}
-                    </Button>
-                </Grid>
+            <Grid container spacing={2} sx={{ width: "100%" }} justifyContent={"end"}>
                 <Grid size={{ xs: 12, md: 2 }}>
                     <Button onClick={onClose} color="error">
                         {getLocalText('Close')}
@@ -27,6 +22,11 @@ const ATFormButtonDialogWrapperDialog = ({ children, onClose, onSubmitClick, onR
                 <Grid size={{ xs: 12, md: 2 }}>
                     <Button onClick={onResetClick} color="warning">
                         {getLocalText('Reset')}
+                    </Button>
+                </Grid>
+                <Grid size={{ xs: 12, md: 2 }}>
+                    <Button onClick={onSubmitClick} color="success">
+                        {getLocalText('Save')}
                     </Button>
                 </Grid>
             </Grid>
